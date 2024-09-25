aa1e547ad7ea53507268abc8d10aebf7192f10c3
chaoyang shen <chaoyangshen@chaoyangdeair.lan>
2024-09-25 16:25:50 -0400

    add pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import "./App.scss";
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import axios from "axios";
 
 import Header from "./components/Header/Header";
 import Next from "./components/Next/Next";
@@ -16,6 +18,12 @@ function App() {
   console.log(filteredVideos);
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/videos/:dynamic-part" element={<VideoDetailPage />} />
+        </Routes>
+      </BrowserRouter>
       <Header />
       <Video video={SelectedVideo} />
       <Comments comments={SelectedVideo.comments} />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,6 @@
 import "./Header.scss";
+import { BrowserRoutee, Routes, Route } from "react-router-dom";
+import axios from "axios";
 
 export default function Header() {
   return (
